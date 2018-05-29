61df5152bb38aa6f4458e1fe38e06ad7b1959bb1
Matthew Buchanan <matt@Matthews-MacBook-Air.local>
2018-05-29 09:59:06 -0400

    we have updated our privacy policy

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
   var burgers = sequelize.define("burgers", {
-    burger_name: DataTypes.STRING,
-    devoured: DataTypes.BOOLEAN
+    burger_name: {
+      type: DataTypes.STRING,
+      notEmpty: true,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    }
   });
   return burgers;
 };
